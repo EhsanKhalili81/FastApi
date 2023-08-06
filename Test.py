8e73b49ede7e0bfdc1e88a2826e102dac293efae
EhsanKhalili81 <126971267+EhsanKhalili81@users.noreply.github.com>
2023-08-06 13:12:48 +0430

    FASTAPI
    Complete

--- Test.py
+++ Test.py
@@ -1,10 +1,12 @@
-from typing import Union
 
 from fastapi import FastAPI
+from uvicorn import run
 
 app = FastAPI()
-
 
 @app.get("/helloworld")
 def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    run(app, host="0.0.0.0", port=80)
